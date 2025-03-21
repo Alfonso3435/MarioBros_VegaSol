af6f941de6b076e31cdd27382a54b01f3cb6477f
Alfonso3435 <141585287+Alfonso3435@users.noreply.github.com>
2025-03-21 13:21:28 -0600

    LogicaMenu
    Se agregó la lógica de navegación en la escena del menú. Incluyendo navegación a otras escenas, botón de cierre de aplicación y aparición y desaparición de componentes al presionar el botón de "Ayuda"

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/*
+Lógia de botones y controles del menú principal. Incluye la navegación a las diferentes secciones del menú.
+Autor: Juan Alfonso Vega Sol
+*/
+public class MenuController : MonoBehaviour
+{
+    private UIDocument menu;
+    private Button botonJugar;
+    private Button botonAyuda;
+    private Button botonCreditos;
+    private Button botonVolver;
+    private Button botonCerrar;
+    private VisualElement contenedorOpciones;
+    private VisualElement contenedorAyuda;
+
+    void OnEnable()
+    {
+        menu = GetComponent<UIDocument>();
+        var root = menu.rootVisualElement;
+
+        botonJugar = root.Q<Button>("Jugar");
+        botonAyuda = root.Q<Button>("Ayuda");
+        botonCreditos = root.Q<Button>("Creditos");
+        botonVolver = root.Q<Button>("Regreso");
+        botonCerrar = root.Q<Button>("Cerrar");
+
+        contenedorOpciones = root.Q<VisualElement>("Opciones");
+        contenedorAyuda = root.Q<VisualElement>("ContenedorAyuda");
+
+        contenedorAyuda.style.display = DisplayStyle.None;
+
+        // Callbacks
+        botonJugar.RegisterCallback<ClickEvent>(evt => IniciarJuego("Juego"));
+        botonCreditos.RegisterCallback<ClickEvent>(evt => IniciarJuego("Creditos"));
+        botonAyuda.RegisterCallback<ClickEvent>(MostrarAyuda);
+        botonVolver.RegisterCallback<ClickEvent>(VolverAlMenu);
+        botonCerrar.RegisterCallback<ClickEvent>(CerrarJuego);
+    }
+
+    private void IniciarJuego(string escena)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(escena);
+    }
+
+    private void MostrarAyuda(ClickEvent evt)
+    {
+        contenedorOpciones.style.display = DisplayStyle.None;
+        contenedorAyuda.style.display = DisplayStyle.Flex;
+    }
+
+    private void VolverAlMenu(ClickEvent evt)
+    {
+        contenedorAyuda.style.display = DisplayStyle.None;
+        contenedorOpciones.style.display = DisplayStyle.Flex;
+    }
+
+    private void CerrarJuego(ClickEvent evt)
+    {
+        UnityEditor.EditorApplication.isPlaying = false; 
+        Application.Quit(); 
+    }
+}
